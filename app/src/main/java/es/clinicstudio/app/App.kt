c7450f4b770e0df38f38c2556a8c871661072187
Víctor Hernández <victor.hernandezbermejo@gmail.com>
2017-10-15 14:34:06 +0200

    Configured Crashlitycs for crash reporting

--- app/src/main/java/es/clinicstudio/app/App.kt
+++ app/src/main/java/es/clinicstudio/app/App.kt
@@ -1,9 +1,11 @@
 package es.clinicstudio.app
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
 import es.clinicstudio.app.di.ApplicationComponent
 import es.clinicstudio.app.di.DaggerApplicationComponent
 import es.clinicstudio.app.di.module.ApplicationModule
+import io.fabric.sdk.android.Fabric
 
 /**
  * Clinic Studio [Application] for Android.
@@ -17,6 +19,9 @@ class App: Application() {
     override fun onCreate() {
         super.onCreate()
         initializeInjector()
+
+        // Initialize Crashlytics
+        Fabric.with(this, Crashlytics())
     }
 
     private fun initializeInjector() {
