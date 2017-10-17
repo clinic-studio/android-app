5d2493062f2a6dc711cbe3d369c7b98f59e5550f
Víctor Hernández <victor.hernandezbermejo@gmail.com>
2017-10-17 21:57:51 +0200

    Provided category repository dependency through the repository module

--- app/src/main/java/es/clinicstudio/app/di/module/RepositoryModule.kt
+++ app/src/main/java/es/clinicstudio/app/di/module/RepositoryModule.kt
@@ -2,7 +2,9 @@ package es.clinicstudio.app.di.module
 
 import dagger.Module
 import dagger.Provides
+import es.clinicstudio.app.data.source.CategoryRepository
 import es.clinicstudio.app.data.source.PostRepository
+import es.clinicstudio.app.data.source.impl.CategoryRepositoryImpl
 import es.clinicstudio.app.data.source.impl.PostRepositoryImpl
 import javax.inject.Singleton
 
@@ -12,8 +14,25 @@ import javax.inject.Singleton
 @Module
 class RepositoryModule {
 
+    /**
+     * Provides a [PostRepository] instance.
+     *
+     * @param[postRepository] Post repository implementation.
+     * @return Post repository instance.
+     */
     @Provides @Singleton
     fun providePostRepository(postRepository: PostRepositoryImpl): PostRepository {
         return postRepository
+    }
+
+    /**
+     * Provides a [CategoryRepository] instance.
+     *
+     * @param[categoryRepository] Category repository implementation.
+     * @return Category repository instance.
+     */
+    @Provides @Singleton
+    fun provideCategoryRepository(categoryRepository: CategoryRepositoryImpl): CategoryRepository {
+        return categoryRepository
     }
 }
